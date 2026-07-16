ef7ab4928aefe13bab5046dadfb51a4e771202a1
= <=>
2026-07-16 17:24:59 -0600

    fix: exception response

--- src/main/java/com/server/app/common/exceptions/response/ExceptionResponse.java
+++ src/main/java/com/server/app/common/exceptions/response/ExceptionResponse.java
@@ -1,8 +1,10 @@
 package com.server.app.common.exceptions.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class ExceptionResponse {
   private final int status;
   private final String message;
